1a684bae8cb80de2bfe2fe68ecf077eb5f102a4d
zgq <zgqst9871@163.com>
2020-08-24 21:13:49 +0800

    deploy

---
+++ precache-manifest.047e4593cd90efbd4a562b6a75cb47fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50746667cc6f9360ff2d",
+    "url": "/book-keeping-website/css/app.29e70bc2.css"
+  },
+  {
+    "revision": "88667ef323b3ade11e15",
+    "url": "/book-keeping-website/css/chunk-vendors.1fa22b09.css"
+  },
+  {
+    "revision": "1b3bf6234878bd4d460285194c0fdd11",
+    "url": "/book-keeping-website/img/upper2.1b3bf623.jpg"
+  },
+  {
+    "revision": "99d4f6162074fc7b77d52792bd05b940",
+    "url": "/book-keeping-website/index.html"
+  },
+  {
+    "revision": "50746667cc6f9360ff2d",
+    "url": "/book-keeping-website/js/app.57087d07.js"
+  },
+  {
+    "revision": "88667ef323b3ade11e15",
+    "url": "/book-keeping-website/js/chunk-vendors.dee4b735.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/book-keeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/book-keeping-website/robots.txt"
+  }
+]);
